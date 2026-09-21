e6d11266690a0a7150f2b22f66bcd51353efcb5f
AbdulSalam <abdulsalam.0302@gmail.com>
2026-09-21 12:18:46 +0500

    initital template

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.flutter_template"
+    namespace = "com.mantic.document.organizer"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -37,7 +37,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_template"
+        applicationId = "com.mantic.document.organizer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/mantic/document/organizer/MainActivity.kt
+++ android/app/src/main/kotlin/com/mantic/document/organizer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_template
+package com.mantic.document.organizer
 
 import io.flutter.embedding.android.FlutterActivity
 
